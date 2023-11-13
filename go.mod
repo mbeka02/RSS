4b86effcb62ecf018ee68781e8e57dc92ca680cc
mbeka02 <antonymbeka@gmail.com>
2023-11-13 18:08:00 +0300

    updated

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/mbeka02/RSS
 
 go 1.21.0
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -2,12 +2,38 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
 )
 
 
 
 func main() {
 	fmt.Println("RSS project")
+	//read env file
+	godotenv.Load(".env")
+
+	 port :=os.Getenv("PORT")
+	 if port == "" {
+		log.Fatal("Port is not set")
+	 }
+	 fmt.Println("Port is set to ", port)
+	 router:= chi.NewRouter()
+
+	 server:= &http.Server{
+		Handler: router,
+		 Addr: ":"+port,
+	 }
+
+	 err:= server.ListenAndServe()
+     if(err!=nil){
+        log.Fatal(err)
+	 }
+
 }
 
  
